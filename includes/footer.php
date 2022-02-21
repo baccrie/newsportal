1d208f86e0d803e65d59c923a8ed9d866c96617e
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-02-21 18:43:29 +0100

    Updated Footer.php file

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; NewPortal <?php echo date('Y');?></p>
+        <p class="m-0 text-center text-white">Copyright &copy; No 1 online news portal <?php echo date('Y');?></p>
       </div>
       <!-- /.container -->
     </footer>
